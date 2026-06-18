fd08ace94178cb54464bc980a71af13d3be99774
Carlos Silva <cd160204@gmail.com>
2026-06-18 15:54:51 -0300

    feat(PercolationStats): cria a implementação do construtor em PercolationStats
    este commit cria uma implementação do construtor na classe PercolationStats, o qual recebe um valor inteiro n e um número de tentativas para simular percolação (trials), realiza trials iterações para criar, abrir e percolar sites em um grid n * n de maneira aleatória e uniforme, determinando o limiar de percolação (threshold) com base na quantidade de sites abertos / n * n

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,7 +1,38 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
+	private static final double CONFIDENCE = 1.96;
+	private final double[] thresholds;
+	private final int trials;
 	
 	// perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+    	if (n <= 0) {
+    		throw new IllegalArgumentException("The value of n should be greater than 0, input: " + n);
+    	}
+    	
+    	if (trials <= 0) {
+    		throw new IllegalArgumentException("The value of trials should be greater than 0, input: " + n);
+    	}
+    	
+    	this.trials = trials;
+    	this.thresholds = new double[trials];
+    	
+    	for (int T = 0; T < trials; T++) {
+    		Percolation p = new Percolation(n);
+    		
+    		while (!p.percolates()) {
+    			int row = StdRandom.uniformInt(1, n + 1);
+    			int col = StdRandom.uniformInt(1, n + 1);
+    			
+    			if (!p.isOpen(row, col)) {
+    				p.open(row, col);
+    			}
+    		}
+    		
+    		thresholds[T] = (double) p.numberOfOpenSites() / (n * n);
+    	}
+    	
     }
 
     // sample mean of percolation threshold
